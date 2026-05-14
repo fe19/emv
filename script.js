f110bb2db9623041425583becaaff2e01f53ab2f
fe <flth741@gmail.com>
2026-05-14 19:58:27 +0200

    initialize sample project

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('changeTextBtn').addEventListener('click', function() {
+    const paragraph = document.querySelector('p');
+    paragraph.textContent = 'You clicked the button! JavaScript is working.';
+});
